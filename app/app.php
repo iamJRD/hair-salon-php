af1ec35931cd5b7913a9e0dabb5c033c241aae69
Jared Beckler <jared.beckler@gmail.com>
2016-02-26 10:28:18 -0800

    add twig template and silex routes for adding/displaying stylists

--- app/app.php
+++ app/app.php
@@ -15,7 +15,19 @@
 	use Symfony\Component\HttpFoundation\Request;
 	Request::enableHttpMethodParameterOverride();
 
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
 
+    $app->post('/add_stylist', function() use ($app) {
+        $name = $_POST['name'];
+        $phone_number = $_POST['phone_number'];
+        $test_stylist = new Stylist($name, $phone_number);
+        $test_stylist->save();
+        $stylists = Stylist::getAll();
+
+        return $app['twig']->render('index.html.twig', array('stylists' => $stylists));
+    });
 
     return $app;
 ?>
