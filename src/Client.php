d102923196c7af5194fd5dacc4fc5d6ef3674e5c
Jared Beckler <jared.beckler@gmail.com>
2016-02-26 10:56:06 -0800

    add Client setters/getters and passing tests

--- src/Client.php
+++ src/Client.php
@@ -1,6 +1,47 @@
 <?php
     class Client
     {
-        
+        private $client_name;
+        private $client_phone_number;
+        private $id;
+        private $stylist_id;
+
+        function __construct($client_name, $client_phone_number, $id = null, $stylist_id)
+        {
+            $this->client_name = $client_name;
+            $this->client_phone_number = $client_phone_number;
+            $this->id = $id;
+            $this->stylist_id = $stylist_id;
+        }
+// SETTERS
+        function setClientName($new_client_name)
+        {
+            $this->client_name = $new_client_name;
+        }
+
+        function setClientPhoneNumber($new_client_phone_number)
+        {
+            $this->client_phone_number = $new_client_phone_number;
+        }
+// GETTERS
+        function getClientName()
+        {
+            return $this->client_name;
+        }
+
+        function getClientPhoneNumber()
+        {
+            return $this->client_phone_number;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function getStylistId()
+        {
+            return $this->stylist_id;
+        }
     }
 ?>
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -14,6 +14,72 @@
 
     class ClientTest extends PHPUnit_Framework_TestCase
     {
+        function testGetClientName()
+        {
+            // Arrange
+            $client_name = 'Jared';
+            $client_phone_number = '5033123649';
+            $id = 1;
+            $stylist_id = 1;
+            $test_client = new Client($client_name, $client_phone_number, $id, $stylist_id);
 
+            // Act
+            $result = $test_client->getClientName();
+
+            // Assert
+            $this->assertEquals($client_name, $result);
+        }
+
+        function testGetClientPhoneNumber()
+        {
+            // Arrange
+            $client_name = 'Jared';
+            $client_phone_number = '5033123649';
+            $id = 1;
+            $stylist_id = 1;
+            $test_client = new Client($client_name, $client_phone_number, $id, $stylist_id);
+
+            // Act
+            $result = $test_client->getClientPhoneNumber();
+
+            // Assert
+            $this->assertEquals($client_phone_number, $result);
+        }
+
+        function testGetId()
+        {
+            // Arrange
+            $client_name = 'Jared';
+            $client_phone_number = '5033123649';
+            $id = 1;
+            $stylist_id = 1;
+            $test_client = new Client($client_name, $client_phone_number, $id, $stylist_id);
+
+            // Act
+            $result = $test_client->getId();
+
+            // Assert
+            $this->assertEquals($id, $result);
+        }
+
+        function testGetStylistId()
+        {
+            // Arrange
+            $stylist_name = 'Ann';
+            $stylist_phone_number = '5555555555';
+            $id = null;
+            $test_stylist = new Stylist($stylist_name, $stylist_phone_number, $id);
+
+            $client_name = 'Jared';
+            $client_phone_number = '5033123649';
+            $stylist_id = $test_stylist->getId();
+            $test_client = new Client($client_name, $client_phone_number, $id, $stylist_id);
+
+            // Act
+            $result = $test_client->getStylistId();
+
+            // Assert
+            $this->assertEquals($stylist_id, $result);
+        }
     }
 ?>
